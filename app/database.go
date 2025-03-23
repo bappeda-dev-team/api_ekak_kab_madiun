6dbcd32d7be3f5fb293a1e8917122f307db0ac0e
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2025-03-23 16:24:03 +0700

    change db

--- app/database.go
+++ app/database.go
@@ -37,12 +37,6 @@ func GetConnection() *sql.DB {
 		connStr = dbUrl
 	}
 
-	// dbUrl := os.Getenv("DB_URL")
-
-	// if dbUrl != "" {
-	// 	connStr = dbUrl
-	// }
-
 	db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("Error membuka koneksi database: %v", err)
