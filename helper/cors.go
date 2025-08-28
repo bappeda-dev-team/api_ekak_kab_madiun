aa03315b85f7e2ae1f1a277bad8d33c20b0fa737
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-08-28 15:48:27 +0700

    corscors

--- helper/cors.go
+++ helper/cors.go
@@ -5,8 +5,8 @@ import "github.com/rs/cors"
 func NewCORSMiddleware() *cors.Cors {
 	return cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
-		AllowedMethods:   []string{"*"},
-		AllowedHeaders:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
 	})
 }
