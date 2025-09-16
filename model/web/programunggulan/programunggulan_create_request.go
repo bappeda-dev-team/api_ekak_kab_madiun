d062bd5abdca5703594b9f0e4615d8284f684cb9
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2025-09-16 20:24:34 +0700

    fix json

--- model/web/programunggulan/programunggulan_create_request.go
+++ model/web/programunggulan/programunggulan_create_request.go
@@ -2,8 +2,8 @@ package programunggulan
 
 type ProgramUnggulanCreateRequest struct {
 	KodeProgramUnggulan       string `json:"kode_program_unggulan"`
-	NamaTagging               string `json:"nama_tagging"`
-	KeteranganProgramUnggulan string `json:"keterangan_program_unggulan"`
+	NamaTagging               string `json:"nama_program_unggulan"`
+	KeteranganProgramUnggulan string `json:"rencana_implementasi"`
 	Keterangan                string `json:"keterangan"`
 	TahunAwal                 string `json:"tahun_awal" validate:"required"`
 	TahunAkhir                string `json:"tahun_akhir" validate:"required"`
--- model/web/programunggulan/programunggulan_response.go
+++ model/web/programunggulan/programunggulan_response.go
@@ -3,8 +3,8 @@ package programunggulan
 type ProgramUnggulanResponse struct {
 	Id                        int     `json:"id"`
 	KodeProgramUnggulan       string  `json:"kode_program_unggulan"`
-	NamaTagging               string  `json:"nama_tagging"`
-	KeteranganProgramUnggulan *string `json:"keterangan_program_unggulan"`
+	NamaTagging               string  `json:"nama_program_unggulan"`
+	KeteranganProgramUnggulan *string `json:"rencana_implementasi"`
 	Keterangan                *string `json:"keterangan"`
 	TahunAwal                 string  `json:"tahun_awal"`
 	TahunAkhir                string  `json:"tahun_akhir"`
