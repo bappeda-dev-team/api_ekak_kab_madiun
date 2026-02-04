990df9010addc846facdb21d0b266ae6bcc8b99e
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2026-02-04 21:13:46 +0700

    fix rekin

--- model/web/rencanakinerja/rencanakinerja_response.go
+++ model/web/rencanakinerja/rencanakinerja_response.go
@@ -6,21 +6,22 @@ import (
 )
 
 type RencanaKinerjaResponse struct {
-	Id                   string                      `json:"id_rencana_kinerja,omitempty"`
-	IdPohon              int                         `json:"id_pohon,omitempty"`
-	NamaPohon            string                      `json:"nama_pohon,omitempty"`
-	LevelPohon           int                         `json:"level_pohon,omitempty"`
-	NamaRencanaKinerja   string                      `json:"nama_rencana_kinerja,omitempty"`
-	TahunAwal            string                      `json:"tahun_awal,omitempty"`
-	TahunAkhir           string                      `json:"tahun_akhir,omitempty"`
-	JenisPeriode         string                      `json:"jenis_periode,omitempty"`
-	Tahun                string                      `json:"tahun,omitempty"`
-	StatusRencanaKinerja string                      `json:"status_rencana_kinerja,omitempty"`
-	Catatan              string                      `json:"catatan,omitempty"`
-	KodeOpd              opdmaster.OpdResponseForAll `json:"operasional_daerah,omitempty"`
-	PegawaiId            string                      `json:"pegawai_id,omitempty"`
-	NamaPegawai          string                      `json:"nama_pegawai,omitempty"`
-	Indikator            []IndikatorResponse         `json:"indikator,omitempty"`
+	Id                    string                      `json:"id_rencana_kinerja,omitempty"`
+	IdPohon               int                         `json:"id_pohon,omitempty"`
+	PerluUbahPohonKinerja bool                        `json:"perlu_ubah_pokin"`
+	NamaPohon             string                      `json:"nama_pohon,omitempty"`
+	LevelPohon            int                         `json:"level_pohon,omitempty"`
+	NamaRencanaKinerja    string                      `json:"nama_rencana_kinerja,omitempty"`
+	TahunAwal             string                      `json:"tahun_awal,omitempty"`
+	TahunAkhir            string                      `json:"tahun_akhir,omitempty"`
+	JenisPeriode          string                      `json:"jenis_periode,omitempty"`
+	Tahun                 string                      `json:"tahun,omitempty"`
+	StatusRencanaKinerja  string                      `json:"status_rencana_kinerja,omitempty"`
+	Catatan               string                      `json:"catatan,omitempty"`
+	KodeOpd               opdmaster.OpdResponseForAll `json:"operasional_daerah,omitempty"`
+	PegawaiId             string                      `json:"pegawai_id,omitempty"`
+	NamaPegawai           string                      `json:"nama_pegawai,omitempty"`
+	Indikator             []IndikatorResponse         `json:"indikator,omitempty"`
 	// SubKegiatan          subkegiatan.SubKegiatanResponse `json:"sub_kegiatan,omitempty"`
 	Action []web.ActionButton `json:"action,omitempty"`
 }
