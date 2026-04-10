401de8631a1172f5eef414e5c5a989576539def7
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-04-10 20:46:43 +0700

    remove duplicate alter table lembaga

--- db/20260401040944_add_nama_kepala_pemda_dan_nip_kepala_pemda_to_tb_lembaga.down.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE tb_lembaga
-  DROP COLUMN nama_kepala_pemda,
-  DROP COLUMN nip_kepala_pemda,
-  DROP COLUMN jabatan_kepala_pemda;
--- db/20260401040944_add_nama_kepala_pemda_dan_nip_kepala_pemda_to_tb_lembaga.up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE tb_lembaga
-  ADD COLUMN nama_kepala_pemda VARCHAR(255),
-  ADD COLUMN nip_kepala_pemda VARCHAR(255),
-  ADD COLUMN jabatan_kepala_pemda VARCHAR(255);
