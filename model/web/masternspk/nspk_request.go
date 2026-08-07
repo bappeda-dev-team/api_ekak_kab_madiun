f08a644a93ab57db6cef5e51413f1f840a760637
zulfikar1609 <83656229+zulfikar1609@users.noreply.github.com>
2026-08-07 18:28:35 +0700

    buat model dan repository master nspk

---
+++ model/web/masternspk/nspk_request.go
@@ -0,0 +1,14 @@
+package masternspk
+
+type NspkRequest struct {
+	KodeOpd string `json:"kode_opd" validate:"required"`
+	Nspk    string `json:"nspk" validate:"required"`
+	Tahun   int    `json:"tahun" validate:"required"`
+}
+
+type PpdUpdateRequest struct {
+	ID      int    `json:"id"`
+	KodeOpd string `json:"kode_opd" validate:"required"`
+	Nspk    string `json:"nspk" validate:"required"`
+	Tahun   int    `json:"tahun" validate:"required"`
+}
