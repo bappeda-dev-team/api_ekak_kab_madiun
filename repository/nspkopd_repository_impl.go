2ccafd857ce24c42636a3203d3fa214ab7718224
zulfikar1609 <83656229+zulfikar1609@users.noreply.github.com>
2026-08-12 19:59:08 +0700

    revisi repository nspk opd findall

--- repository/nspkopd_repository_impl.go
+++ repository/nspkopd_repository_impl.go
@@ -190,7 +190,7 @@ func (repository *NspkOpdRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx
 			   isu.id_nspk, 
 			   mn.nspk, 
 			   isu.id_tujuan_opd, 
-			   to.tujuan, 
+			   tto.tujuan, 
 			   isu.id_sasaran_opd, 
 			   so.nama_sasaran_opd, 
 			   isu.tahun
@@ -202,8 +202,8 @@ func (repository *NspkOpdRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx
 		LEFT JOIN tb_master_nspk mn
 		ON mn.id = isu.id_nspk
 
-		LEFT JOIN tb_tujuan_opd to
-		ON to.id = isu.id_tujuan_opd
+		LEFT JOIN tb_tujuan_opd tto
+		ON tto.id = isu.id_tujuan_opd
 
 		LEFT JOIN tb_sasaran_opd so
 		ON so.id = isu.id_sasaran_opd
