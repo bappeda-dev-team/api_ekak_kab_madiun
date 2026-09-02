6feddb21078ebdb4a31841a7778805cabe55ebf3
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-09-03 02:20:09 +0700

    fix duplicate route

--- app/router.go
+++ app/router.go
@@ -256,9 +256,6 @@ func NewRouter(
 	router.GET("/pohon_kinerja_opd/pokinpemda_review/:id", pohonKinerjaOpdController.FindidPokinWithAllTema)
 	router.GET("/pohon_kinerja/cetak/:id", pohonKinerjaAdminController.CetakPokin)
 
-	//report tematik
-	router.GET("/listOpdTematik/:idPokin", pohonKinerjaAdminController.FindPokinAdminByIdHierarkiOpdView)
-
 	// isustrategis - csf
 	router.GET("/isustrategis/csfs/:tahun", csfController.AllCsfsByTahun)
 	router.GET("/isustrategis/csf/detail/:id", csfController.FindById)
