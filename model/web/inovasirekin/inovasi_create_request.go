befa13929f14840fd2a5525e9e66ba37a82db756
zulfikar1609 <83656229+zulfikar1609@users.noreply.github.com>
2026-09-16 17:41:44 +0700

    revisi model inovasi rekin

--- model/web/inovasirekin/inovasi_create_request.go
+++ model/web/inovasirekin/inovasi_create_request.go
@@ -1,13 +1,11 @@
 package inovasirekin
 
-import "time"
-
 type InovasiRekinCreateRequest struct {
 	RekinId           string 	  `json:"rencana_kinerja_id"`
 	KodeOpd           string 	  `json:"kode_opd"`
 	NamaInovasi       string 	  `json:"nama_inovasi"`
 	JenisInovasiId    int   	  `json:"jenis_inovasi_id"`
-	WaktuImplementasi time.Time   `json:"waktu_implementasi"`
+	WaktuImplementasi string      `json:"waktu_implementasi"`
 	Instansi          string 	  `json:"instansi"`
 	Inovator          string 	  `json:"inovator"`
 }
--- model/web/inovasirekin/inovasi_update_request.go
+++ model/web/inovasirekin/inovasi_update_request.go
@@ -1,12 +1,10 @@
 package inovasirekin
 
-import "time"
-
 type InovasiRekinUpdateRequest struct {
 	Id                string    `json:"id"`
 	NamaInovasi       string    `json:"nama_inovasi"`
 	JenisInovasiId    int       `json:"jenis_inovasi_id"`
-	WaktuImplementasi time.Time `json:"waktu_implementasi"`
+	WaktuImplementasi string    `json:"waktu_implementasi"`
 	Instansi          string    `json:"instansi"`
 	Inovator          string    `json:"inovator"`
 }
