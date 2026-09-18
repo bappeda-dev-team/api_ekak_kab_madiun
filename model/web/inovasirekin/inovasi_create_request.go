7d1b70881f43f85bd849d370a50a5f19262bd861
zulfikar1609 <83656229+zulfikar1609@users.noreply.github.com>
2026-09-18 16:03:55 +0700

    menambahkan validasi required untuk form inovasi rekin

--- model/web/inovasirekin/inovasi_create_request.go
+++ model/web/inovasirekin/inovasi_create_request.go
@@ -1,11 +1,11 @@
 package inovasirekin
 
 type InovasiRekinCreateRequest struct {
-	RekinId           string 	  `json:"rencana_kinerja_id"`
-	KodeOpd           string 	  `json:"kode_opd"`
-	NamaInovasi       string 	  `json:"nama_inovasi"`
-	JenisInovasiId    int   	  `json:"jenis_inovasi_id"`
-	WaktuImplementasi string      `json:"waktu_implementasi"`
-	Instansi          string 	  `json:"instansi"`
-	Inovator          string 	  `json:"inovator"`
+	RekinId           string `json:"rencana_kinerja_id" validate:"required"`
+	KodeOpd           string `json:"kode_opd" validate:"required"`
+	NamaInovasi       string `json:"nama_inovasi" validate:"required"`
+	JenisInovasiId    int    `json:"jenis_inovasi_id" validate:"required"`
+	WaktuImplementasi string `json:"waktu_implementasi" validate:"required"`
+	Instansi          string `json:"instansi"`
+	Inovator          string `json:"inovator" validate:"required"`
 }
--- model/web/inovasirekin/inovasi_update_request.go
+++ model/web/inovasirekin/inovasi_update_request.go
@@ -1,10 +1,10 @@
 package inovasirekin
 
 type InovasiRekinUpdateRequest struct {
-	Id                string    `json:"id"`
-	NamaInovasi       string    `json:"nama_inovasi"`
-	JenisInovasiId    int       `json:"jenis_inovasi_id"`
-	WaktuImplementasi string    `json:"waktu_implementasi"`
-	Instansi          string    `json:"instansi"`
-	Inovator          string    `json:"inovator"`
+	Id                string `json:"id"`
+	NamaInovasi       string `json:"nama_inovasi" validate:"required"`
+	JenisInovasiId    int    `json:"jenis_inovasi_id" validate:"required"`
+	WaktuImplementasi string `json:"waktu_implementasi" validate:"required"`
+	Instansi          string `json:"instansi"`
+	Inovator          string `json:"inovator" validate:"required"`
 }
